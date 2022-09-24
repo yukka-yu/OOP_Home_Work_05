dc1fb344005464bf920f5c313741d61e61e10fff
yukka-yu <97976485+yukka-yu@users.noreply.github.com>
2022-09-24 11:31:54 +0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ package home5;
 
 import java.io.IOException;
 
-public class Main {
+public class Main { ЧЧЧ
     public static void main(String[] args) throws IOException {
         Presentor presentor = new Presentor(new View());
         presentor.Button();
